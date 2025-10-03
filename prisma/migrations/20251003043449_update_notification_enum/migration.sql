699b10625d18271a3f86169d7f3b44bea26057a3
Dylan Walsh <dylanwitsend@gmail.com>
2025-10-02 23:19:26 -0700

    DELETE FROM "Notification" WHERE type = 'FOLLOW'; to make migrate work

--- prisma/migrations/20251003043449_update_notification_enum/migration.sql
+++ prisma/migrations/20251003043449_update_notification_enum/migration.sql
@@ -6,6 +6,7 @@
 */
 -- AlterEnum
 BEGIN;
+DELETE FROM "Notification" WHERE type = 'FOLLOW';
 CREATE TYPE "public"."NotificationType_new" AS ENUM ('LIKE', 'COMMENT', 'SUBMIT');
 ALTER TABLE "public"."Notification" ALTER COLUMN "type" TYPE "public"."NotificationType_new" USING ("type"::text::"public"."NotificationType_new");
 ALTER TYPE "public"."NotificationType" RENAME TO "NotificationType_old";

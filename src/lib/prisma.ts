df0e0ca3f604316964dea8f7fa7bfc6c9d31d813
Dylan Walsh <dylanwitsend@gmail.com>
2025-12-03 23:14:07 -0800

    update to prisma v7

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,17 @@
 // lib/prisma.ts
-import { PrismaClient } from "@/generated/prisma"; // ✅ key change necessary to beat bug
+import { PrismaClient } from "@/generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
